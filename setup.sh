3b4c9e50ce782487af690711d7c81d8bf5a5d531
Kotaro Kikuchi <kotaro@pcl.cs.waseda.ac.jp>
2016-05-11 03:25:47 +0900

    Update setup.sh
    add neobundle

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 # install pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
+# install NeoBundle
+# You should do ":NeoBundleInstall" on vim
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
